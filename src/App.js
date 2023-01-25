b2f9f39e823231c87a597d3ec5c4419811d3fff7
Jessica H <jessicahebert75@gmail.com>
2023-01-25 10:37:49 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div>
-      Our App
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>Trini Treasures</h1>
+        </Container>
+      </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        'id': '1',
+        'name': 'Blue earrings',
+        'image': 'https://images.pexels.com/photos/1413420/pexels-photo-1413420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'description': 'Handcrafted blue earrings',
+        'category': 'jewelry',
+        'price': 15.99,
+        'countInStock' : 1
+    },
+    {       
+        'id': '2',
+        'name': 'Natural round earrings',
+        'image': 'https://images.pexels.com/photos/13595744/pexels-photo-13595744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'description': 'Handcrafted natural round shaped earrings',
+        'category': 'jewelry',
+        'price': 15.99,
+        'countInStock' : 1
+    },
+    {       
+        'id': '3',
+        'name': 'Painting 1',
+        'image': 'https://images.pexels.com/photos/12868017/pexels-photo-12868017.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'description': 'Handcrafted painting',
+        'category': 'paintings',
+        'price': 100.00,
+        'countInStock' : 1
+    },
+]
+
+export default products
