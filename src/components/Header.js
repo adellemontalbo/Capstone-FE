3f0dd2c854f5abdca3110ff925121379d84af377
Jessica H <jessicahebert75@gmail.com>
2023-01-30 11:48:10 -0800

    working on productscreen

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="me-auto">
+                  <Nav className="mr-auto">
                   <LinkContainer to={'/cart'}>
                     <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
                   </LinkContainer>
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,17 +1,30 @@
-import React,{useEffect} from 'react'
+import React,{useState,useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import {Container, Image,ListGroup,Button,Card, ListGroupItem } from 'react-bootstrap'
-import products from '../products'
+// import products from '../products'
+import axios from 'axios'
 
+// const ProductScreen=() => {
+//     const params = useParams();
+//     const {id} = params;
+//     const product =products.find((product) => product.id === id);
 
-const ProductScreen=() => {
-    const params = useParams();
-    const {id} = params;
-    const product =products.find((product) => product.id === id);
+//     const{image,name,price,description} = product
+function ProductScreen({match}) {
+    console.log(match)
+    const [product, setProduct] = useState([])
+    
+    useEffect(()=>{
+        async function fetchProducts(){
+            const {data}= await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProducts()
 
-    const{image,name,price,description} = product
+    },[])
+
     return (
 
         <div>
