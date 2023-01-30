0aff6437690a3d6058e7b7097b519552bcaa47ca
Jessica H <jessicahebert75@gmail.com>
2023-01-30 12:10:15 -0800

    added productScreen changes

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -3,27 +3,27 @@ import { Link, useParams } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import {Container, Image,ListGroup,Button,Card, ListGroupItem } from 'react-bootstrap'
-// import products from '../products'
+import products from '../products'
 import axios from 'axios'
 
-// const ProductScreen=() => {
-//     const params = useParams();
-//     const {id} = params;
-//     const product =products.find((product) => product.id === id);
+const ProductScreen=() => {
+    const params = useParams();
+    const {id} = params;
+    const product =products.find((product) => product.id === id);
 
-//     const{image,name,price,description} = product
-function ProductScreen({match}) {
-    console.log(match)
-    const [product, setProduct] = useState([])
+    const{image,name,price,description} = product
+// function ProductScreen({match}) {
+//     console.log(match)
+//     const [product, setProduct] = useState([])
     
-    useEffect(()=>{
-        async function fetchProducts(){
-            const {data}= await axios.get(`/api/products/${match.params.id}`)
-            setProduct(data)
-        }
-        fetchProducts()
+//     useEffect(()=>{
+//         async function fetchProducts(){
+//             const {data}= await axios.get(`/api/products/${match.params.id}`)
+//             setProduct(data)
+//         }
+//         fetchProducts()
 
-    },[])
+    // },[])
 
     return (
 
