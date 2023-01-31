f5acd059992c11dff31143a404388d565036484c
Jessica H <jessicahebert75@gmail.com>
2023-01-31 11:09:34 -0800

    working on product screen add to cart

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -2,16 +2,16 @@ import React,{useState,useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import {Container, Image,ListGroup,Button,Card, ListGroupItem } from 'react-bootstrap'
+import {Container, Image,ListGroup,Button,Card, ListGroupItem,Form } from 'react-bootstrap'
 import products from '../products'
 import axios from 'axios'
 
-// import React from 'react'
 
 const ProductScreen = () => {
     const{id} = useParams();
     const [product, setProduct] = useState({})
-    
+    const [qty, setqty] =useState(1)
+
     useEffect(() => {
         axios.get(`/api/products/${id}`)
         .then(response => {
@@ -22,18 +22,20 @@ const ProductScreen = () => {
         })
         .catch(error=>console.log(error))
     },)
-  
+    const addToCartHandler = ()=>{
+       history.push 
+    }
     return (
         <div>
             
             <Link to='/' className='btn btn-dark my-3'>Go Back</Link>
                 <Row>
-                    <Col md={8}>
+                    <Col md={6}>
                         <img height={500} width={500} src={product.image} alt= {product.image}/>
                     </Col>
         
             
-            <Col md={2}>
+            <Col md={3}>
                 <ListGroup variant='flush'>
                     <ListGroup.Item>
                         <h3> {product.name}</h3>
@@ -47,7 +49,7 @@ const ProductScreen = () => {
                 </ListGroupItem>
             </Col>
         
-            <Col md={2}>
+            <Col md={3}>
                 <Card>
                     <ListGroup variant='flush'>
                         <ListGroupItem>
@@ -66,8 +68,29 @@ const ProductScreen = () => {
                                 </Col>
                             </Row>
                         </ListGroupItem>
+                        {product.countInStock > 0 && (
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Qty</Col>
+                                    <Col xs ='auto' className='my-1'>
+                                    <Form.Control
+                                        as ="select"
+                                        value={qty}
+                                        onChange={(e)=> setqty(e.target.value)}>
+                                        {
+                                            [...Array(product.countInStock).keys()].map((x)=>(
+                                                <option key={x+1} value={x+1}>
+                                                    {x+1}
+                                                </option>
+                                            ))
+                                        }   
+                                        </Form.Control>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                        )}
                         <ListGroupItem>
-                            <Button className='btn-block' disabled={product.countInStock=== 0} type='button'> Add to Cart</Button>
+                            <Button onClick={addToCartHandler} className='btn-block' disabled={product.countInStock=== 0} type='button'> Add to Cart</Button>
                         </ListGroupItem>
                     </ListGroup>
                 </Card>
