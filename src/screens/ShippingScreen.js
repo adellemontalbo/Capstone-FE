e484c3f7e9ac4db84ca7b714b5f0342b5fc13579
Adelle Montalbo <adellemontalbo@gmail.com>
2023-02-02 14:29:37 -0800

    added checkoutsteps component, gives import error

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 // import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../actions/cartActions' 
+import { CheckoutSteps } from '../components/CheckoutSteps'
 
 function ShippingScreen() {
     const navigate = useNavigate();
@@ -27,13 +28,15 @@ function ShippingScreen() {
     //TO REMOVE WHEN I HAVE FORM CONTAINER:
     return(
         <div>
+            {/* <CheckoutSteps step1 step2 /> */}
             SHIPPING
         </div>
     )}
 
-//TO ADD WHEN FORM CONTAINER MADE
+// TO ADD WHEN FORM CONTAINER MADE
 //     return (
-//         <FormContainer>
+//         <FormContainer> 
+//             <CheckoutSteps step1 step2 />
 //             <h1>Shipping</h1>
 //             <Form onSubmit={submitHandler}>
 
