1472c3967e0acfc89d1af8807521e883d33787c8
Adelle Montalbo <adellemontalbo@gmail.com>
2023-02-03 11:00:41 -0800

    styling changes

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -92,8 +92,7 @@ function CartScreen() {
                 <Card>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
-                            <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
-                        </h2>  
+                            <h2>Items ({cartItems.reduce((acc, item) => acc + item.qty, 0)})</h2>  
                         ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
                         </ListGroup.Item>
                     </ListGroup>
