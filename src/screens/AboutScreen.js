5579f63cd79a9790c625008bac427d2cee7c574a
Adelle Montalbo <adellemontalbo@gmail.com>
2023-02-10 18:45:32 -0800

    adeed photo in about screen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -5,7 +5,7 @@ function AboutScreen() {
   return (
     <div>
       <h2 className='About'>About The Artist</h2>
-      <Image src=''></Image>
+      <Image src='https://images.pexels.com/photos/5376665/pexels-photo-5376665.jpeg?auto=compress&cs=tinysrgb&w=600'></Image>
       <p>Marjorie is the creator of all products on this website *unless we run out of time and there will be stock images*. She lives in Trinidad and Tobago.</p>
     </div>
   )
